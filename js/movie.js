22adda1f38afa565d1446db0f12c7fd1c3f84bc5
ringo-117 <168697072+ringo-117@users.noreply.github.com>
2024-09-06 11:47:13 +0900

    update_15
    ヘッドスパとドライヘッドスパを合わせました。（文章やモーダル内も変更しています。）
    フッター内の診療時間の表を修正しました。表の下に＊お昼休みなしの追加。
    DIARYの記事とリンクを追加しました。

--- js/movie.js
+++ js/movie.js
@@ -1,25 +1,4 @@
 
-
-//　MOVIE
-// document.querySelectorAll(".modal_open_movie").forEach
-// ((video) => {
-// 	video.onclick = () => {
-// 		document.querySelector(".modal_movie").style.
-// 		display = "block";
-// 		document.querySelector(".modal_movie video").play();
-// 	}
-// });
-// const span = (document.querySelector(".modal_movie span").onclick = () => {
-// 	document.querySelector(".modal_movie").style.
-// 	display = "none";
-// 	document.querySelector(".modal_movie video").pause();
-// });
-// const div = (document.querySelector(".modal_movie div").onclick = () => {
-// 	document.querySelector(".modal_movie").style.
-// 	display = "none";
-// 	document.querySelector(".modal_movie video").pause();
-// });
-
 //　MOVIE
 document.querySelectorAll(".modal_open_movie").forEach
 ((video) => {
